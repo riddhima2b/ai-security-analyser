d779ab9d30e5b41084ff35e11dc8d5dfd84a021a
RIDDHIMA BHALLA <rid28bhalla@gmail.com>
2026-09-25 21:38:00 +0530

    main file

--- app/main.py
+++ app/main.py
@@ -1,21 +1,29 @@
 from fastapi import FastAPI
-from scanners.semgrep_scanner import scan
+from scanners.semgrep_scanner import semgrepScan
+from scanners.gitleaks_scanner import gitLeaksScan
 from test_gemini import analyser
-
+from cloner import clone_repo
+import shutil 
 app = FastAPI(title = "AI Security Vulnerability Analyser")
 
 @app.get("/")
 def root():
     return {"message": "Welcome to the AI Security Vulnerability Analyser API!"}
 
-path = input("Enter the path to scan: ")
+path = input("Enter the repo to scan: ")
 
-findings = scan(path)
+cloned = clone_repo(path)
+findings = {
+    "semgrep":semgrepScan(cloned),
+    "secrets": gitLeaksScan(cloned)
+    }
 
-if not findings:
+
+if not any(findings.values()):
     print("No issues found")
     exit()
 
 ai_response = analyser(findings)
 
 print(ai_response)
+shutil.rmtree(cloned)
