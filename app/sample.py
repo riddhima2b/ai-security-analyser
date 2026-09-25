3de56bbef19d769f60732babb056ba9745ff4299
RIDDHIMA BHALLA <rid28bhalla@gmail.com>
2026-09-25 13:39:59 +0530

    fake data to check leaks

--- app/sample.py
+++ app/sample.py
@@ -1,10 +1,3 @@
-import subprocess
-import sqlite3
-
-user_input = input("Enter command: ")
-
-subprocess.run(user_input, shell=True)
-
-conn = sqlite3.connect("app.db")
-query = "SELECT * FROM users WHERE name = '" + user_input + "'"
-conn.execute(query)
+# fake data
+mysql_user = "admin"
+password_sql = "Nbr57svw5hy43"
